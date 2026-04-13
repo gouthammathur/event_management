c2283692b22d6db3d4165daa16044697b465aa3a
Goutham Mathur <gouthammathur0928@gmail.com>
2026-04-13 00:02:10 +0000

    fix server for mobile access and updates

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.get("/registrations", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on http://0.0.0.0:5000");
 });
